62245363eab2f7ef19fd2a057c0b15c3bdc2c472
Choain <cangiecho@gmail.com>
2023-03-22 16:47:07 +0900

    Dividend PRJ

---
+++ src/main/java/com/dividend/scraper/Scraper.java
@@ -0,0 +1,9 @@
+package com.dividend.scraper;
+
+import com.dividend.model.Company;
+import com.dividend.model.ScrapedResult;
+
+public interface Scraper {
+    Company scrapCompanyByTicker(String ticker);
+    ScrapedResult scrap(Company company);
+}
